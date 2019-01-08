ca830b883b83fa44139b66b7e2e0590b82e271fc
lpiem <Loic-0607@hotmail.fr>
2019-01-08 14:03:06 +0100

    [FEAT] Init project

---
+++ app/src/main/java/com/example/lpiem/muse_app_android/CaptureListActivity.java
@@ -0,0 +1,13 @@
+package com.example.lpiem.muse_app_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CaptureListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture_list);
+    }
+}
