a21c73de8aec3af3456bf7b155e8d26f578830c8
Morgane B <morgane0798@hotmail.fr>
2019-01-14 15:12:15 +0100

    [FEAT] Added chronometer

--- app/src/main/java/com/example/lpiem/muse_app_android/presentation/ui/activity/NewCaptureActivity.java
+++ app/src/main/java/com/example/lpiem/muse_app_android/presentation/ui/activity/NewCaptureActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.lpiem.muse_app_android.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class NewCaptureActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnDetails;
@@ -20,6 +26,10 @@ public class NewCaptureActivity extends AppCompatActivity implements View.OnClic
     ImageButton btn3d;
     FloatingActionButton addCapture;
 
+    private Chronometer chronometer;
+    private long pauseOffset;
+    private boolean running;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +37,7 @@ public class NewCaptureActivity extends AppCompatActivity implements View.OnClic
         this.setTitle(R.string.new_capture_title_bar);
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        chronometer = findViewById(R.id.chronometer);
         btnDetails = findViewById(R.id.btnDetails);
         btnDetails.setOnClickListener(this);
         btnStart = findViewById(R.id.btnStart);
@@ -68,14 +79,37 @@ public class NewCaptureActivity extends AppCompatActivity implements View.OnClic
             case R.id.btnStart:
                 btnStart.setVisibility(View.INVISIBLE);
                 btnStop.setVisibility(View.VISIBLE);
+                if (!running) {
+                    chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+                    chronometer.start();
+                    running = true;
+                }
                 break;
             case R.id.btnStop:
                 btnStop.setVisibility(View.INVISIBLE);
                 btnStart.setVisibility(View.VISIBLE);
+                if (running) {
+                    chronometer.stop();
+                    pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+                    running = false;
+                }
                 break;
+            // TODO : faire icône reset
+            // case R.id.btnReset:
+//            btnStop.setVisibility(View.INVISIBLE);
+//            btnStart.setVisibility(View.VISIBLE);
+//                chronometer.setBase(SystemClock.elapsedRealtime());
+//                pauseOffset = 0;
+//                break;
             case R.id.fab_addCapture:
-                Intent intent = new Intent(NewCaptureActivity.this, CaptureListActivity.class);
-                startActivity(intent);
+                Bundle extras = getIntent().getExtras();
+                String currentDate = DateFormat.getDateInstance().format(new Date());
+                boolean isInserted = db.insertData(extras.getString("nom"), extras.getString("description"), currentDate, "04:25", extras.getInt("idEtat"),"donnees");
+                if (isInserted == true) {
+                    Toast.makeText(NewCaptureActivity.this, "Capture ajoutée", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(NewCaptureActivity.this, CaptureListActivity.class);
+                    startActivity(intent);
+                }
                 break;
             case R.id.btn3D:
                 // TODO : activer la 3D
