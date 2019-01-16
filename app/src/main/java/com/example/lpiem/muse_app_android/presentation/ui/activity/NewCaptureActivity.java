98f84ee6cec831e13455decc0e5f282e4e52d5ae
theo <theo.tanchoux@etu.univ-lyon1.fr>
2019-01-16 11:55:39 +0100

    [FEAT] Add speed graph

--- app/src/main/java/com/example/lpiem/muse_app_android/presentation/ui/activity/NewCaptureActivity.java
+++ app/src/main/java/com/example/lpiem/muse_app_android/presentation/ui/activity/NewCaptureActivity.java
@@ -2,21 +2,32 @@ package com.example.lpiem.muse_app_android.presentation.ui.activity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import com.example.lpiem.muse_app_android.R;
 import com.example.lpiem.muse_app_android.presentation.presenter.NewCapturePresenter;
 import com.example.lpiem.muse_app_android.presentation.ui.listener.ConnectionListener;
 import com.example.lpiem.muse_app_android.presentation.ui.listener.DataListener;
 import com.example.lpiem.muse_app_android.presentation.ui.view.NewCaptureView;
+import com.github.anastr.speedviewlib.PointerSpeedometer;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.lang.ref.WeakReference;
@@ -24,7 +35,7 @@ import java.lang.ref.WeakReference;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class NewCaptureActivity extends AppCompatActivity implements View.OnClickListener, NewCaptureView {
+public class NewCaptureActivity extends AppCompatActivity implements View.OnClickListener, NewCaptureView, OnChartValueSelectedListener {
 
     private NewCapturePresenter presenter = new NewCapturePresenter(this);
 
@@ -37,6 +48,10 @@ public class NewCaptureActivity extends AppCompatActivity implements View.OnClic
     private DataListener dataListener;
 
     private final Handler handler = new Handler();
+
+    private LineChart chart;
+
+    private PointerSpeedometer pointerSpeedometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,11 +70,15 @@ public class NewCaptureActivity extends AppCompatActivity implements View.OnClic
         addCapture.setOnClickListener(this);
         btn3d = findViewById(R.id.btn3D);
         btn3d.setOnClickListener(this);
+        pointerSpeedometer = findViewById(R.id.monitor1);
+
+
+        realtimeChart();
+        speedometer();
 
         presenter.setContextMuseManager(this);
 
         WeakReference<NewCapturePresenter> weakPresenter = new WeakReference<>(presenter);
-
 
         dataListener = new DataListener(weakPresenter);
 
@@ -82,11 +101,6 @@ public class NewCaptureActivity extends AppCompatActivity implements View.OnClic
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_new_capture, menu);
-        return true;
-    }
 
     @Override
     public void onClick(View v) {
@@ -127,6 +141,49 @@ public class NewCaptureActivity extends AppCompatActivity implements View.OnClic
         Log.d("mlk", "test eegBuffer 3 : "+String.format("%6.2f", eegBuffer[2]));
         Log.d("mlk", "test eegBuffer 4 : "+String.format("%6.2f", eegBuffer[3]));
 
+        LineData data = chart.getData();
+
+        if (data != null) {
+
+            ILineDataSet set = data.getDataSetByIndex(0);
+            ILineDataSet set1 = data.getDataSetByIndex(1);
+            ILineDataSet set2= data.getDataSetByIndex(2);
+            ILineDataSet set3 = data.getDataSetByIndex(3);
+            // set.addEntry(...); // can be called as well
+
+            if (set == null && set1 == null && set2 == null && set3 == null) {
+                set = createSet(ColorTemplate.getHoloBlue(), "Capteur 1");
+                set1 = createSet(Color.GREEN, "Capteur 2");
+                set2 = createSet(Color.RED, "Capteur 3");
+                set3 = createSet(Color.YELLOW, "Capteur 4");
+                data.addDataSet(set);
+                data.addDataSet(set1);
+                data.addDataSet(set2);
+                data.addDataSet(set3);
+            }
+
+            data.addEntry(new Entry(set.getEntryCount(), (float) eegBuffer[0]), 0);
+            data.addEntry(new Entry(set1.getEntryCount(), (float) eegBuffer[1]), 1);
+            data.addEntry(new Entry(set2.getEntryCount(), (float) eegBuffer[2]), 2);
+            data.addEntry(new Entry(set3.getEntryCount(), (float) eegBuffer[3]), 3);
+            data.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            chart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            chart.setVisibleXRangeMaximum(120);
+            // chart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            chart.moveViewToX(data.getEntryCount());
+
+            // this automatically refreshes the chart (calls invalidate())
+            // chart.moveViewTo(data.getXValCount()-7, 55f,
+            // AxisDependency.LEFT);
+        }
+
+        pointerSpeedometer.speedTo((float) eegBuffer[0], 0);
     }
 
     @Override
@@ -159,5 +216,92 @@ public class NewCaptureActivity extends AppCompatActivity implements View.OnClic
     protected void onStop() {
         super.onStop();
         presenter.stopHandler();
+    }
+
+    private void speedometer() {
+        pointerSpeedometer.setUnit("");
+        pointerSpeedometer.setMaxSpeed(1600f);
+        pointerSpeedometer.setTicks(1600f);
+    }
+
+    private void realtimeChart() {
+        chart = findViewById(R.id.graph_capture);
+        chart.setOnChartValueSelectedListener(this);
+
+        // enable description text
+        chart.getDescription().setEnabled(true);
+
+        // enable touch gestures
+        chart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setDrawGridBackground(false);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        chart.setPinchZoom(true);
+
+        // set an alternative background color
+        chart.setBackgroundColor(Color.TRANSPARENT);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+
+        // add empty data
+        chart.setData(data);
+
+        // get the legend (only possible after setting data)
+        Legend l = chart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.WHITE);
+        l.setTextSize(35f);
+        l.setFormSize(35f);
+        l.setXEntrySpace(20f);
+
+        XAxis xl = chart.getXAxis();
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(false);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+        xl.removeAllLimitLines();
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setAxisMaximum(1600f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
+    }
+
+    private LineDataSet createSet(int color, String label) {
+
+        LineDataSet set = new LineDataSet(null, label);
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(color);
+        set.setCircleColor(Color.WHITE);
+        set.setLineWidth(2f);
+        set.setCircleRadius(1.5f);
+        set.setFillAlpha(65);
+        set.setFillColor(color);
+        set.setHighLightColor(Color.rgb(244, 117, 117));
+        set.setValueTextColor(Color.WHITE);
+        set.setValueTextSize(9f);
+        set.setDrawValues(false);
+        return set;
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        Log.i("Entry selected", e.toString());
+    }
+
+    @Override
+    public void onNothingSelected() {
+        Log.i("Nothing selected", "Nothing selected.");
     }
 }
